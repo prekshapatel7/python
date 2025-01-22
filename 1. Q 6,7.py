06748575589931952717c260536c862fe8bcb055
prekshapatel7 <preksha.samik.patel@gmail.com>
2025-01-22 16:42:17 +0530

    Add files via upload

---
+++ 1. Q 6,7.py
@@ -0,0 +1,10 @@
+#Convert hours into minutes and  Convert minutes into hour , (question 6 and 7)
+def min_hour(hours):
+    inminutes=60*hours
+    print("the given hours in minutes gives the value",inminutes)
+
+min_hour(2)
+def hour_min(minutes):
+    inhours=minutes/60
+    print("the given minutes correspond to hours=",inhours)
+hour_min(180)
