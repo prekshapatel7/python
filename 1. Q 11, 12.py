9ff426a075c1f3b7c4627587e6b038337ee1c417
prekshapatel7 <preksha.samik.patel@gmail.com>
2025-01-22 19:09:27 +0530

    Add files via upload

---
+++ 1. Q 11, 12.py
@@ -0,0 +1,12 @@
+#. Convert grams into kg. Convert kgs into grams , questions 11, 12
+def gram_kg():
+    gm= int(input("enter the value in grams:"))
+    kg=gm/1000
+    print("the value in kg is :",kg)
+gram_kg()
+def kg_grams():
+    kg= int(input("enter the value in kg:"))
+    mg=kg*1000
+    print("the value in mg is :",mg)
+kg_grams()
+
