05584ff9c76db98d39ce599a87a332c415d3a90a
prekshapatel7 <preksha.samik.patel@gmail.com>
2025-01-22 18:51:13 +0530

    Add files via upload

---
+++ 1. Q10.py
@@ -0,0 +1,6 @@
+#Convert dollars into pound where 1 $ = 48 Rs. And 1 pound = 70 Rs
+def dollars_pounds():
+    p=float(input("enter the value in dollars:"))
+    pounds=p*1.24
+    print("the value in pounds is ",pounds)
+dollars_pounds()
