90df326f5665418f5ab6cfa436fe210697d05f80
prekshapatel7 <preksha.samik.patel@gmail.com>
2025-01-22 18:37:09 +0530

    Add files via upload

---
+++ 1. Q 8,9.py
@@ -0,0 +1,14 @@
+#Convert dollars into Rs. Where 1 $ = 48 Rs Convert Rs. into dollars where 1 $ = 48 Rs.
+def dollars_rs():
+    a=int(input("enter the amount in dollars"))
+    rs=a*48
+    print("the vaue in rs is",rs)
+dollars_rs()
+
+def rs_dollars():
+    a=int(input("enter the amount in rupees"))
+    dollars=a/48
+    print("the vaue in dollars is",dollars)
+rs_dollars()
+
+      
