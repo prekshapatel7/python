2ca9401761067c9dc6eeac0a0e3fcb71eb3cef19
prekshapatel7 <preksha.samik.patel@gmail.com>
2025-01-29 11:42:40 +0530

    Add files via upload

---
+++ 2. Q5.py
@@ -0,0 +1,8 @@
+#Accept age of a person. If age is less than 18, print minor otherwise Major.
+def age():
+    age=int(input("enter the age"))
+    if age<18:
+        print("minor")
+    else :
+        print("major")
+age()
