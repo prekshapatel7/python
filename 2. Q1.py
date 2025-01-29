75dfacdc10989e4b4ad53a00c2725aa1fb592e54
prekshapatel7 <preksha.samik.patel@gmail.com>
2025-01-29 11:25:13 +0530

    Add files via upload

---
+++ 2. Q1.py
@@ -0,0 +1,9 @@
+#print the largest and smallest value out of two
+def largestsmallest():
+    a=int(input("enter the first number"))
+    b=int(input("enter the second number"))
+    if a>b:
+        print("the largest number is ",a)
+    else:
+        print("the smallest number is",b)
+largestsmallest()
