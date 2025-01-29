3ceae8bab3fd7a67686f71dd5b3f555fa9a987fc
prekshapatel7 <preksha.samik.patel@gmail.com>
2025-01-29 11:37:25 +0530

    Add files via upload

---
+++ 2. Q3.py
@@ -0,0 +1,8 @@
+#Check whether a given number is odd or even.
+def evenodd():
+    a=int(input("eneter the number"))
+    if a%2==0:
+        print("even")
+    else:
+        print("odd")
+evenodd()
