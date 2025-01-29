30c99a9750e4a400880079158377579ba55561c5
prekshapatel7 <preksha.samik.patel@gmail.com>
2025-01-29 11:39:23 +0530

    Add files via upload

---
+++ 2. Q4.py
@@ -0,0 +1,8 @@
+#Check whether a given number is divisible by 10 or not
+def evenodd():
+    a=int(input("eneter the number"))
+    if a%10==0:
+        print("divisible")
+    else:
+        print("not divisible")
+evenodd()
