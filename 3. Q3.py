491aa0baf1220a935ae46340961113d368c9b78b
prekshapatel7 <preksha.samik.patel@gmail.com>
2025-02-04 19:07:07 +0530

    Add files via upload

---
+++ 3. Q3.py
@@ -0,0 +1,11 @@
+#Accept two strings. Check whether one string is there in another string.
+def strings():
+    string1=input("enter the string 1")
+    string2=input("enter the string 2")
+    if string2 in string1:
+        print("yes")
+    else:
+        print("no")
+strings()
+
+    
