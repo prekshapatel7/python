eb6d9f10f3d0c04ede49e250484596cc59303944
prekshapatel7 <preksha.samik.patel@gmail.com>
2025-02-11 22:32:48 +0530

    Add files via upload

---
+++ 4. Q1.py
@@ -0,0 +1,8 @@
+#Print all alphabets in upper case and in lower case.
+import string
+
+# Printing uppercase alphabets
+print("Uppercase alphabets:", string.ascii_uppercase)
+
+# Printing lowercase alphabets
+print("Lowercase alphabets:", string.ascii_lowercase)
