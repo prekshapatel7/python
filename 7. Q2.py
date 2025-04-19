be13f0434db8d3383b4075a8dcdd2fef29bd9c03
prekshapatel7 <preksha.samik.patel@gmail.com>
2025-04-19 07:38:00 +0530

    Add files via upload

---
+++ 7. Q2.py
@@ -0,0 +1,9 @@
+#Write a program to check whether a dictionary is empty or not.
+d = { }
+
+if not d:# not d means if d is empty
+
+    print("Empty Dictionary")
+
+print(bool(d))
+#since it is empty bool will return false
