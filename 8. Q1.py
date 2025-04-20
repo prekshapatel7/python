f5b7b5661110e59911ebec201ad7c6eae43be527
prekshapatel7 <preksha.samik.patel@gmail.com>
2025-04-20 08:05:36 +0530

    Add files via upload

---
+++ 8. Q1.py
@@ -0,0 +1,5 @@
+#Write a program that converts words present in a list
+#into uppercase and stores them in a set.
+list1=['preksha','cse','harsh']
+set1=set(items.upper() for items in list1)
+print(set1)
